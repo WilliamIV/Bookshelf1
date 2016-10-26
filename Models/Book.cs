854f7e6ca0c17ec899d11c0b9951e33612b0b173
WilliamIV <william.green.iv@gmail.com>
2016-10-26 14:21:25 -0400

    initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf1.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Publish Date")]
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+       [ForeignKey("Category")]
+       //[Display (Name = "Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
